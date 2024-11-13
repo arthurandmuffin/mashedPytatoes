800dadf3106085f7da03fae3a10e113c5cc6cb72
Arthur <56365708+ChubbyWaddlingPenguin@users.noreply.github.com>
2024-11-12 20:22:44 -0500

    fix: wrong place for statics

--- utils/movement.py
+++ utils/movement.py
@@ -1,80 +1,61 @@
 import time
 import math
+from . import statics
 from utils import brick
 from utils import BP, EV3UltrasonicSensor, Motor, TouchSensor, wait_ready_sensors
 from BrickPi import *
 
-# set up
-MOTOR_POLL_DELAY = 0.05
-US_SENSOR = EV3UltrasonicSensor(2) 
-LEFT_MOTOR = Motor("A")
-RIGHT_MOTOR = Motor("D")
-
-POWER_LIMIT = 80
-SPEED_LIMIT = 270
-TRN_SPD = 90
-
-RB = 0.11  # Half distance between wheels, need to measure in lab
-RW = 0.028  # Wheel radius
-DISTTODEG = (180 / (math.pi * RW))
-ORIENTTODEG = RB / RW
-
 def wait_for_motor(Motor):
     # Wait for motor to spin or slow down
     while math.isclose(motor.get_speed(), 0):
-        time.sleep(MOTOR_POLL_DELAY)
+        time.sleep(statics.MotorPollDelay)
     while not math.isclose(motor.get_speed, 0):
-        time.sleep(MOTOR_POLL_DELAY)
+        time.sleep(statics.MotorPollDelay)
 
 def init_motor(Motor):
         motor.reset_encoder()
-        motor.set_limits(POWER_LMIT, SPEED_LIMIT)
-        motor.set_power(0)
-
-def init_motor(Motor):
-        motor.reset_encoder()
-        motor.set_limits(POWER_LMIT, SPEED_LIMIT)
+        motor.set_limits(statics.MotorPowerLimit, statics.MotorSpeedLimit)
         motor.set_power(0)
 
 def moveForward(distance, speed):
     LEFT_MOTOR.set_dps(speed)
     RIGHT_MOTOR.set_dps(speed)
 
-    LEFT_MOTOR.set_limits(POWER_LIMIT, speed)
-    RIGHT_MOTOR.set_limits(POWER_LIMITS, speed)
+    LEFT_MOTOR.set_limits(statics.MotorPowerLimit, speed)
+    RIGHT_MOTOR.set_limits(statics.MotorPowerLimit, speed)
 
-    LEFT_MOTOR.set_position_relative(int(distance*DISTTODEG))
-    RIGHT_MOTOR.set_position_relative(int(distance*DISTTODEG))
+    LEFT_MOTOR.set_position_relative(int(distance*statics.DistToDeg))
+    RIGHT_MOTOR.set_position_relative(int(distance*statics.DistToDeg))
 
     wait_for_motor(RIGHT_MOTOR)
 
 
 def moveForwardUntilObstacle():
     while US_SENSOR.get_distance() > 0.1:
-        time.sleep(MOTOR_POLL_DELAY)
+        time.sleep(statics.MotorPollDelay)
     
     stopMotors()
 
-def moveBackward(speed):
+def moveBackward(distance, speed):
      LEFT_MOTOR.set_dps(speed)
      RIGHT_MOTOR.set_dps(speed)
 
-     LEFT_MOTOR.set_limits(POWER_LIMIT, speed)
-     RIGHT_MOTOR.set_limits(POWER_LIMITS, speed)
+     LEFT_MOTOR.set_limits(statics.MotorPowerLimit, speed)
+     RIGHT_MOTOR.set_limits(statics.MotorPowerLimit, speed)
 
-     LEFT_MOTOR.set_position_relative(-int(distance*DISTTODEG))
-     RIGHT_MOTOR.set_position_relative(-int(distance*DISTTODEG))
+     LEFT_MOTOR.set_position_relative(-int(distance*statics.DistToDeg))
+     RIGHT_MOTOR.set_position_relative(-int(distance*statics.DistToDeg))
 
      wait_for_motor(RIGHT_MOTOR)
           
           
 
 def rotate(angle):
-    LEFT_MOTOR.set_limits(POWER_LIMIT, TRN_SPD)
-    RIGHT_MOTOR.set_limits(POWER_LIMIT, TRN_SPD)
+    LEFT_MOTOR.set_limits(statics.MotorPowerLimit, statics.MotorTurnSpeed)
+    RIGHT_MOTOR.set_limits(statics.MotorPowerLimit, statics.MotorTurnSpeed)
 
-    LEFT_MOTOR.set_position_relative(-int(angle * ORIENTTODEG))
-    RIGHT_MOTOR.set_position_relative(int(angle * ORIENTTODEG))
+    LEFT_MOTOR.set_position_relative(-int(angle * statics.OrientToDeg))
+    RIGHT_MOTOR.set_position_relative(int(angle * statics.OrientToDeg))
     wait_for_motor(RIGHT_MOTOR)
 
 def stopMotors():
---
+++ utils/statics.py
@@ -0,0 +1,9 @@
+MotorPollDelay = 0.05
+USSensorPort = 2
+LeftMotorPort = "A"
+RightMotorPort = "D"
+MotorPowerLimit = 80
+MotorSpeedLimit = 270
+MotorTurnSpeed = 90
+WheelRadius = 0.028 #check
+HalfWheelBase = 0.11 #check
